97b20600eb1dc5df9b3f73865b306f46a27ac236
unknown <Administrateur@TURING-B23.formationara.local>
2022-01-24 16:32:42 +0100

    added random iungredient selection

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +16,21 @@ class RecetteController extends AbstractController
         return $this->render('recette/index.html.twig', [
             'controller_name' => 'RecetteController',
         ]);
+    }
+
+    #[Route('/rand', name: 'generate_recette')]
+    public function generate(EntityManagerInterface $entityManager ): Response
+    {
+        $ingredients = $entityManager->getRepository(Ingredient::class)->findAll();
+        $rand = rand(2,4);
+        $selected_ingredients_index = array_rand($ingredients,$rand);
+        $selected_ingredients = [];
+        for ($i = 0; $i < $rand ;$i++) {
+            array_push($selected_ingredients,$ingredients[$selected_ingredients_index[$i]]);
+        }
+        return $this->render('recette/random.html.twig', [
+            'controller_name' => 'RecetteController',"ingredients"=>$selected_ingredients
+        ]);
+
     }
 }
